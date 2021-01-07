03b8c1d33db169bd7194ad632d56901cbaaa205d
Brian Merritt <Brian.Merritt@jhuapl.edu>
2021-01-07 11:00:42 -0500

    tab update

--- client/src/modules/server/api/modules/docker.js
+++ client/src/modules/server/api/modules/docker.js
@@ -84,7 +84,9 @@ export class DockerObj {
 						let exists;
 						exists = await $this.check_container_exists(name)
 						if (!exists){
+							logger.info(1)
 							const opt  = await $this.obj.build(params);//build up the params for docker to run appropriately
+							logger.info(2)
 							options = opt.options;
 							command = opt.command
 							store.docker.createContainer({
