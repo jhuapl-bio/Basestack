1581ac8af9fc7e9505e71515dd25f96d239ad1e0
Brian Merritt <Brian.Merritt@jhuapl.edu>
2021-01-26 15:13:25 -0500

    remove validate require for thourhgput and drift

--- client/src/renderer/components/NavbarModules/BasestackConsensus/BasestackConsensus.vue
+++ client/src/renderer/components/NavbarModules/BasestackConsensus/BasestackConsensus.vue
@@ -935,22 +935,12 @@ export default {
         			}
         		},
         		specifics: {
-        			throughput: {
-        				exists: {
-        					checked: value => value === true
-        				}
-        			},
         			seq_summary: {
         				exists: {
         					checked: value => value === true 
         				},
         				errors: {
         					checked: value => value && value.length == 0 
-        				}
-        			},
-        			drift_correction: {
-        				exists: {
-        					checked: value => value === true
         				}
         			}
         		},
