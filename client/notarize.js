e075e5af630b693ddeafe0c717f4cca1d263e254
Merritt-Brian <50592701+Merritt-Brian@users.noreply.github.com>
2021-10-11 20:03:56 -0400

    update notarize, allow windows and linux build

--- client/notarize.js
+++ client/notarize.js
@@ -12,8 +12,8 @@ exports.default = async function notarizing(context) {
   return await notarize({
     appBundleId: 'com.yourcompany.yourAppId',
     appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS,
+    appleApiKey: process.env.API_KEY_ID,
+    appleApiIssuer: process.env.API_KEY_ISSUER_ID,
     ascProvider: 'TV5X8WHGW3'
   });
 };
