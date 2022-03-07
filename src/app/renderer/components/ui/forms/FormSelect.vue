f3ec725d2969b351a54bee7d945d04b5132dabd8
Tim Long <tim.long@jhuapl.edu>
2022-03-07 11:08:02 -0500

    add form select component

---
+++ src/app/renderer/components/ui/forms/FormSelect.vue
@@ -0,0 +1,15 @@
+<template>
+    <select class="px-3 py-2 border-2 rounded focus:outline-none text-blue-extradark border-gray focus:border-blue-light" @change="$emit('update:options', '$event.target.value')">
+        <option>Choose an option</option>
+        <option>Option 1</option>
+        <option>Option 2</option>
+        <option>Option 3</option>
+    </select>
+</template>
+
+<script>
+export default {
+    props: ['options'],
+    emits: ['update:options']
+}
+</script>
