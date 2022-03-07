724b44d0c662800470d93f5f5819e9e55cc7b110
Tim Long <tim.long@jhuapl.edu>
2022-03-07 09:01:49 -0500

    Add Text Input

---
+++ src/app/renderer/components/ui/forms/TextInput.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="relative">
+        <input 
+            v-bind="$attrs"
+            :value="text"
+            @input="$emit('update:text', '$event.target.value')"
+            class="flex w-full px-4 py-2 border-2 focus:outline-none focus:border-blue-lighter font-body focus:bg-white bg-gray-light border-gray-dark text-gray-darkest placeholder:text-gray-darker"
+        />
+    </div>
+    
+</template>
+
+<script>
+export default {
+    emits: ['update:text'],
+    props: ['text'],
+    computed: {
+        value: () => {
+            return this.text
+        }
+    }
+}
+</script>
