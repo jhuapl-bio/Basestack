c031f6fd1ab12db0375249f4de6e414845a617b3
Allen Hansrisuk <ahansrisuk@gmail.com>
2022-03-10 15:04:32 -0500

    ui details on system summary

--- src/app/renderer/components/Dashboard/System/SystemSummary.vue
+++ src/app/renderer/components/Dashboard/System/SystemSummary.vue
@@ -1,48 +1,66 @@
 <template>
   <div>
-      <div class="markup-h5 strong mb-4">System Summary</div>
-      <div class="border border-gray rounded">
-          <div class="p-4">
-            <h3 class="markup-h5 strong mb-4">Processor</h3>
-            <div class="grid grid-cols-3">
-              <DataLabel  label="CPU Brand" data="Core i7-7820HQ" class="col-span-2"/>
-              <DataLabel  label="Manufacturer" data="Intel"/>
-              <DataLabel  label="Cores" data="8"/>
-              <DataLabel  label="Physical Cores" data="4"/>
-              <DataLabel  label="Virtualization" data="Yes"/>
-            </div>
-          </div>
-          <hr class="border-gray"/>
-          <div class="p-4">
-            <h3 class="markup-h5 strong mb-4">Memory</h3>
-            <div class="grid grid-cols-3">
-              <DataLabel  label="Total Mem (GB)" data="17.18"/>
-              <DataLabel  label="Using Mem (GB)" data="5.43"/>
-              <DataLabel  label="Available Mem (GB)" data="11.70"/>
-            </div>
-          </div>
-          <hr class="border-gray"/>
-          <div class="p-4">
-            <h3 class="markup-h5 strong mb-4">Operating System</h3>
-            <div class="flex justify-between flex-wrap">
-              <DataLabel  label="Kernel" data="20.6.0"/>
-              <DataLabel  label="Platform" data="darwin"/>
-              <DataLabel  label="Distro" data="macOS"/>
-              <DataLabel  label="Release" data="11.5.2"/>
-            </div>
-          </div>
+    <div class="flex items-center justify-between mb-4">
+      <div class="flex items-center">
+        <font-awesome-icon
+          icon="cog"
+          class="text-lg text-blue-extradark mr-2"
+        />
+        <div class="markup-h5 strong">System Summary</div>
       </div>
+      <UncontainedButton
+        label="All System Details"
+        class="markup-h5"
+        icon="chevron-right"
+      />
+    </div>
+    <div class="border border-gray rounded">
+      <div class="p-4">
+        <h3 class="markup-h5 strong mb-4">Processor</h3>
+        <div class="grid grid-cols-3">
+          <DataLabel
+            label="CPU Brand"
+            data="Core i7-7820HQ"
+            class="col-span-2"
+          />
+          <DataLabel label="Manufacturer" data="Intel" />
+          <DataLabel label="Cores" data="8" />
+          <DataLabel label="Physical Cores" data="4" />
+          <DataLabel label="Virtualization" data="Yes" />
+        </div>
+      </div>
+      <hr class="border-gray" />
+      <div class="p-4">
+        <h3 class="markup-h5 strong mb-4">Memory</h3>
+        <div class="grid grid-cols-3">
+          <DataLabel label="Total Mem (GB)" data="17.18" />
+          <DataLabel label="Using Mem (GB)" data="5.43" />
+          <DataLabel label="Available Mem (GB)" data="11.70" />
+        </div>
+      </div>
+      <hr class="border-gray" />
+      <div class="p-4">
+        <h3 class="markup-h5 strong mb-4">Operating System</h3>
+        <div class="flex justify-between flex-wrap">
+          <DataLabel label="Kernel" data="20.6.0" />
+          <DataLabel label="Platform" data="darwin" />
+          <DataLabel label="Distro" data="macOS" />
+          <DataLabel label="Release" data="11.5.2" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import DataLabel from '../../ui/DataLabel.vue'
+import DataLabel from "../../ui/DataLabel.vue";
+import UncontainedButton from "../../ui/buttons/UncontainedButton.vue";
+
 export default {
-    name: 'SystemSummary',
-    components: {
-      DataLabel
-    }
-
-
-}
+  name: "SystemSummary",
+  components: {
+    DataLabel,
+    UncontainedButton,
+  },
+};
 </script>
--- src/app/renderer/components/ui/buttons/UncontainedButton.vue
+++ src/app/renderer/components/ui/buttons/UncontainedButton.vue
@@ -1,20 +1,19 @@
 <template>
-    <button class="flex items-center bg-white button small text-blue-extradark hover:bg-blue-extralight">
-        <div v-if="label">
-            {{label}}
-        </div>
-
-        <div v-if="icon">
-            <font-awesome-icon :icon="icon" />
-        </div>
-    </button>
+  <button
+    class="bg-white button small text-blue-extradark hover:bg-blue-extralight"
+  >
+    <div class="flex items-center space-x-2">
+      <span v-if="label">{{ label }}</span>
+      <font-awesome-icon v-if="icon" :icon="icon" />
+    </div>
+  </button>
 </template>
 
 <script>
 export default {
-    props: {
-        label: {type: String},
-        icon: {type: String, default: 'false'}
-    },
-}
+  props: {
+    label: { type: String },
+    icon: { type: String, default: "false" },
+  },
+};
 </script>
