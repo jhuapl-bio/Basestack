5e6ab06a62222f6ce04aa85d92ae9e22f1fd1cf2
Allen Hansrisuk <ahansrisuk@gmail.com>
2022-03-14 13:21:48 -0400

    create Contact section

---
+++ src/app/renderer/components/Dashboard/Contact.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="flex items-center mb-4">
+      <font-awesome-icon
+        icon="phone-alt"
+        class="text-lg text-blue-extradark mr-2"
+      />
+      <div class="markup-h5 strong">Contact Us</div>
+    </div>
+    <div class="border border-gray rounded">
+      <div class="p-4 grid grid-cols-2">
+        <div class="space-y-4">
+          <h3 class="markup-h5 strong">Basestack Developers</h3>
+          <div>
+            <h4 class="markup-h5">Brian Merrit</h4>
+            <a href="" class="text-blue-ultradark markup-h6">
+              brian.merrit@jhuapl.edu
+            </a>
+          </div>
+          <div>
+            <h4 class="markup-h5">Thomas Mehoke</h4>
+            <a href="" class="text-blue-ultradark markup-h6">
+              thomas.mehoke@jhuapl.edu
+            </a>
+          </div>
+        </div>
+        <div class="space-y-4">
+          <h3 class="markup-h5 strong">Third Party Apps</h3>
+          <div>
+            <a href="" class="text-blue-ultradark markup-h6 strong block">
+              Nextrain</a
+            >
+            <a href="" class="text-blue-ultradark markup-h6 strong block">
+              Artic Network/RAMPART
+            </a>
+            <a href="" class="text-blue-ultradark markup-h6 strong block">
+              IGV
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contact",
+};
+</script>
+
+<style>
+</style>
