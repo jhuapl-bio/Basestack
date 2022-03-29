88b956223cacc46b19f24a4ea386be34467f3171
Tim Long <tim.long@jhuapl.edu>
2022-03-29 11:31:28 -0400

    modulebutton tweaks

--- src/app/renderer/components/ui/nav/ModuleButton.vue
+++ src/app/renderer/components/ui/nav/ModuleButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="rounded shadow-lg inline-flex items-center w-[55px] h-[55px] p-1 bg-white hover:scale-125 duration-500">
+    <button class="rounded shadow-lg inline-flex items-center w-[55px] h-[55px] p-1 bg-white hover:scale-105 duration-500">
         <div class="flex items-center justify-center w-full h-full rounded bg-pink/25">
             <slot>
                 <span class="uppercase markup-h3 text-pink">
