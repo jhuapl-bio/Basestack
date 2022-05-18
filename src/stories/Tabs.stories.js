6ad6ae2ecb0c0d8fd8dcc40ec2b2ee30e2c6f469
Allen Hansrisuk <ahansrisuk@gmail.com>
2022-05-18 13:49:04 -0400

    fix Tab stories

--- src/stories/Tabs.stories.js
+++ src/stories/Tabs.stories.js
@@ -1,47 +1,45 @@
 import Tabs from "@/components/ui/Tabs";
-import Tab from "@/components/ui/Tab"
+import Tab from "@/components/ui/Tab";
 
 export default {
-    title: "Tabs",
-    component: Tabs,
-    subcomponents: { Tab },
-    argTypes: {
-        tabs: {
-            type: 'object',
-            defaultValue: [
-                {
-                    name: 'My Procedures',
-                    selected: true,
-                    content: "Tab #1"
-                },
-                {
-                    name: 'My Modules',
-                    selected: false,
-                    content: "Tab #2"
-                },
-                {
-                    name: 'Browse All Modules',
-                    selected: false,
-                    content: "Tab #3"
-                },
-            ],
-        }
-    }
-}
-
-
-const Template = (args, { argTypes }) => {
-    console.log({args: args, argTypes: argTypes, props: Object.keys(argTypes)})
-    return {
-        props: Object.keys(argTypes),
-        components: { Tabs },
-        subcomponents: { Tab },
-        template: `
-            <Tabs>
-                <Tab v-for="(tab, index) in $props.tabs" :key="index" :v-slot="tab.content" />
-            </Tabs>
-            `,
-    }
+  title: "Tabs",
+  component: Tabs,
+  subcomponents: { Tab },
+  argTypes: {
+    tabs: {
+      type: "object",
+      defaultValue: [
+        {
+          name: "My Procedures",
+          selected: true,
+          content: "Tab #1",
+        },
+        {
+          name: "My Modules",
+          selected: false,
+          content: "Tab #2",
+        },
+        {
+          name: "Browse All Modules",
+          selected: false,
+          content: "Tab #3",
+        },
+      ],
+    },
+  },
 };
 
-export const Default = Template.bind({});
+export const Default = (args, { argTypes }) => {
+  console.log({ args: args, argTypes: argTypes, props: Object.keys(argTypes) });
+  return {
+    props: Object.keys(argTypes),
+    components: { Tabs, Tab },
+    template: `
+            <Tabs>
+                <Tab v-for="(tab, index) in $props.tabs" :key="index" :name="tab.name">
+                    <template v-slot>{{tab.content}}</template>
+                </Tab>
+            </Tabs>
+            `,
+  };
+};
