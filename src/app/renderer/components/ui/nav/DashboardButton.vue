5f46510cfcda229a5ac80f368b8b0b73668b0c98
Tim Long <tim.long@jhuapl.edu>
2022-05-19 14:19:45 -0400

    update markup-label font

--- src/app/renderer/components/ui/nav/DashboardButton.vue
+++ src/app/renderer/components/ui/nav/DashboardButton.vue
@@ -1,6 +1,6 @@
 <template>
     <router-link to="/" class="text-white bg-blue-900 hover:bg-blue-1000 duration-500 h-[105px] w-full flex flex-col items-center justify-center space-y-2">
         <font-awesome-icon icon="border-all" class=" w-[35px] h-[35px]" />
-        <span class="tracking-widest markup-label">Dashboard</span>
+        <span class="tracking-widest markup-label uppercase">Dashboard</span>
     </router-link>
 </template>
