97d36d0484d7961ab7f712ffb0b7103af6e95dea
Tim Long <tim.long@jhuapl.edu>
2022-05-19 14:18:59 -0400

    add new checkbox component

---
+++ src/app/renderer/components/ui/forms/Checkbox.vue
@@ -0,0 +1,13 @@
+<template>
+    <input type="checkbox" value="" name="" class="
+        form-checkbox
+        w-[20px] h-[20px] 
+        bg-gray-300 text-blue-600 
+        border-gray-500 border-2 rounded
+        checked:ring-2 checked:ring-blue-200 checked:shadow-transparent
+        focus-visible:outline-none
+    " 
+    
+    :class="[disabled ? 'shadow-transparent pointer-events-none' : 'shadow-checkbox']"
+    />
+</template>
