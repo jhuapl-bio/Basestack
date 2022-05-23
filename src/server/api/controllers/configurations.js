0d61b3be2d4f8452f2cfbae6ce73b83d90da771a
Merritt-Brian <50592701+Merritt-Brian@users.noreply.github.com>
2022-05-23 10:20:55 -0400

    update mytax and gamma pre-ship config

--- src/server/api/controllers/configurations.js
+++ src/server/api/controllers/configurations.js
@@ -9,7 +9,7 @@ const YAML = require("js-yaml")
  
 export function bytesToSize(bytes) {
    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-   if (bytes == 0) return '0 Byte';        
+   if (bytes == 0) return '0 Byte';         
    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
 }  
