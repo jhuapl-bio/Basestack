b786d1bffad91f37c148b105d5f473ee44b788ca
Tim Long <tim.long@jhuapl.edu>
2022-06-09 12:41:56 -0400

    dynamic padding based on fill value

--- src/app/renderer/components/ui/Alert.vue
+++ src/app/renderer/components/ui/Alert.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="markup-h6 font-normal text-left flex items-center justify-start px-6 py-2 rounded-sm" :class="classes.background">
+    <div class="markup-h6 font-normal text-left flex items-center justify-start rounded-sm" :class="classes.background">
         <font-awesome-icon :icon="icon" class="text-sm mr-2" :class="classes.icon" />
         <span :class="classes.message">{{message}}</span>
     </div>
@@ -28,25 +28,25 @@ export default {
                 switch(props.type) {
                     case 'success':
                         return {
-                            background: 'bg-gray-200',
+                            background: 'bg-gray-200 px-6 py-3',
                             icon: 'text-green',
                             message: 'text-gray-800'
                         }
                     case 'warning':
                         return {
-                            background: 'bg-orange-100',
+                            background: 'bg-orange-100 px-6 py-3',
                             icon: 'text-orange',
                             message: 'text-orange-900'
                         }
                     case 'error':
                         return {
-                            background: 'bg-red-100',
+                            background: 'bg-red-100 px-6 py-3',
                             icon: 'text-red',
                             message: 'text-red-900'
                         }
                     default:
                          return {
-                            background: 'bg-gray-200',
+                            background: 'bg-gray-200 px-6 py-3',
                             icon: 'text-gray-500',
                             message: 'text-gray-800'
                         }
@@ -68,7 +68,7 @@ export default {
             }
             
             return {
-                background: '',
+                background: 'py-2',
                 message: 'text-gray-700',
                 icon: noFillIconColor()
             }
