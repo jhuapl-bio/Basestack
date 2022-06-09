ee7387581ed9aabdc53593b4cc8d874c92351063
Tim Long <tim.long@jhuapl.edu>
2022-06-09 12:34:28 -0400

    Build Alert component with all variants
    - Fill and No Fill
    - success, warning, error, and default

---
+++ src/app/renderer/components/ui/Alert.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="markup-h6 font-normal text-left flex items-center justify-start px-6 py-2 rounded-sm" :class="classes.background">
+        <font-awesome-icon :icon="icon" class="text-sm mr-2" :class="classes.icon" />
+        <span :class="classes.message">{{message}}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['type', 'message', 'fill'],
+    computed: {
+        icon: (props) => {
+            switch(props.type) {
+                case 'success':
+                    return 'check-circle'
+                case 'warning':
+                case 'error':
+                    return 'exclamation-triangle'
+                default:
+                    return ''
+            }
+        },
+
+        classes: (props) => {
+
+            // alerts w/ background colors
+            if(props.fill) {
+                switch(props.type) {
+                    case 'success':
+                        return {
+                            background: 'bg-gray-200',
+                            icon: 'text-green',
+                            message: 'text-gray-800'
+                        }
+                    case 'warning':
+                        return {
+                            background: 'bg-orange-100',
+                            icon: 'text-orange',
+                            message: 'text-orange-900'
+                        }
+                    case 'error':
+                        return {
+                            background: 'bg-red-100',
+                            icon: 'text-red',
+                            message: 'text-red-900'
+                        }
+                    default:
+                         return {
+                            background: 'bg-gray-200',
+                            icon: 'text-gray-500',
+                            message: 'text-gray-800'
+                        }
+                }
+            }
+
+            // alerts w/o background color (only the icon changes)
+            const noFillIconColor = () => {
+                switch(props.type) {
+                    case 'success':
+                        return 'text-green'
+                    case 'warning':
+                        return 'text-orange'
+                    case 'error':
+                        return 'text-red'
+                    default:
+                        return 'text-blue'
+                }
+            }
+            
+            return {
+                background: '',
+                message: 'text-gray-700',
+                icon: noFillIconColor()
+            }
+        }
+    },
+}
+</script>
