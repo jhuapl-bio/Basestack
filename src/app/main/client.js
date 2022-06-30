f237e6daaa617575b92b184346bacda2743a33e4
Allen Hansrisuk <ahansrisuk@gmail.com>
2022-06-30 11:22:48 -0400

    update dependencies

--- src/app/main/client.js
+++ src/app/main/client.js
@@ -1,264 +1,273 @@
+import { ClientMenu } from "./menu";
+import { Updater } from "./updater";
+const { ipcMain, BrowserWindow, dialog, shell, Menu } = require("electron");
+const path = require("path");
 
-import { ClientMenu } from "./menu"
-import { Updater } from "./updater"
-const { ipcMain, BrowserWindow, dialog, shell, Menu } = require('electron')
-const path = require("path")
- 
-export class  Client {
-	constructor(app){
-		this.logger = null
-        this.mainWindow = null
-        this.app = app
-        const isMac = process.platform === 'darwin'
-        const isWin = process.platform === "win32"
+export class Client {
+  constructor(app) {
+    this.logger = null;
+    this.mainWindow = null;
+    this.app = app;
+    const isMac = process.platform === "darwin";
+    const isWin = process.platform === "win32";
 
-        if (!process.env.APPDATA){
-            process.env.APPDATA = this.app.getPath('userData')
-        }
-        if (isMac){
-            process.env.platform_os = "mac"
-        } else if(isWin){
-            process.env.platform_os = "win"
-        } else {
-            process.env.platform_os = "linux"
-        }
-        this.system={
-            'isMac': isMac,
-            'isWin': isWin
-        }
-        const $this = this
-        this.spawned_logs = function spawned_logs(bat, config){
-            bat.stderr.on('data', (data) => {
-              console.log(data.toString());
-              if (config.throwError){
-                throw new Error(data.toString())
-              }
-            });
-            bat.stdout.on('data', (data) => {
-              console.log(`${data.toString()}`)
-            });
-            bat.on('exit', (code) => {
-              let message = `${config.process} exited with code: ${code}`
-              if(config.throwExit){
-                $this.mainWindow.webContents.send('mainNotification', {
-                    icon: (code == 0 ? `info` : `error`),
-                    message: (code == 0 ? `${config.process} succeeded` : `${config.process} failed`),
-                    disable_popup: true
-                })
-              } 
-              console.log(message);
-            });
-        }
-	}
-    updatePort(port){
-      console.log(`updating port to ${port}`)
-      process.env.PORT_SERVER = port
-      this.mainWindow.webContents.send("changePort", port)
-    } 
-    createMenu(){  
-        // let menu = makeMenu(this.this.logger)  
-        console.log("creating menu")
-         
-        let menu = new ClientMenu(this.logger, this.mainWindow, dialog, this.app, this.system, this.spawned_logs, this.updater)
-        menu.store = this.store
-        let m = menu.makeMenu() 
+    if (!process.env.APPDATA) {
+      process.env.APPDATA = this.app.getPath("userData");
     }
-    createUpdater(){ 
-        this.updater = new Updater(this.logger, this.mainWindow, dialog)
-        this.updater.defineUpdater()
+    if (isMac) {
+      process.env.platform_os = "mac";
+    } else if (isWin) {
+      process.env.platform_os = "win";
+    } else {
+      process.env.platform_os = "linux";
     }
-    createWindow () {
-        /**
-         * Initial window options
-         */
-        const $this = this  
-        
-        if (process.env.NODE_ENV !== 'production'){
-            let icon = path.join(__dirname, "..", "static", "img")
-            if (process.env.platform_os == "linux"){
-              icon = path.join(icon, "/icon_1024x1024.png")
-            } else if (process.env.platform_os == 'win'){
-              icon = path.join(icon, "/basestack.ico")
-            } else {
-              icon = path.join(icon, "/basestack.icns")
-            }
-          this.mainWindow = new BrowserWindow({
-            height: 1000,
-            useContentSize: true,
-            width: 1080,
-            title: "Basestack", 
-            webPreferences: { zoomFactor: 0.83, webSecurity: true,enableRemoteModule: true, nodeIntegration:true, worldSafeExecuteJavaScript: true},
-            icon: icon
-          }) 
-           console.log(icon,  __dirname)
-        } else { 
-          let icon = path.join(__dirname, "icons")
-            if (process.env.platform_os == "linux"){
-              icon = path.join(icon, "/icon_1024x1024.png")
-            } else if (process.env.platform_os == 'win'){
-              icon = path.join(icon, "/basestack.ico")
-            } else {
-              icon = path.join(icon, "/basestack.icns")
-            }
-            console.log(__dirname,"DIRNAME")
-          this.mainWindow = new BrowserWindow({
-            height: 1000, 
-            useContentSize: true,
-            width: 1080,
-            title: "Basestack",
-            webPreferences: {zoomFactor: 0.83,  webSecurity: true,enableRemoteModule: true, nodeIntegration:true, worldSafeExecuteJavaScript: true},
-            icon: icon
-          })
+    this.system = {
+      isMac: isMac,
+      isWin: isWin,
+    };
+    const $this = this;
+    this.spawned_logs = function spawned_logs(bat, config) {
+      bat.stderr.on("data", (data) => {
+        console.log(data.toString());
+        if (config.throwError) {
+          throw new Error(data.toString());
         }
-       
-        this.mainWindow.webContents.session.clearCache()
+      });
+      bat.stdout.on("data", (data) => {
+        console.log(`${data.toString()}`);
+      });
+      bat.on("exit", (code) => {
+        let message = `${config.process} exited with code: ${code}`;
+        if (config.throwExit) {
+          $this.mainWindow.webContents.send("mainNotification", {
+            icon: code == 0 ? `info` : `error`,
+            message:
+              code == 0
+                ? `${config.process} succeeded`
+                : `${config.process} failed`,
+            disable_popup: true,
+          });
+        }
+        console.log(message);
+      });
+    };
+  }
+  updatePort(port) {
+    console.log(`updating port to ${port}`);
+    process.env.PORT_SERVER = port;
+    this.mainWindow.webContents.send("changePort", port);
+  }
+  createMenu() {
+    // let menu = makeMenu(this.this.logger)
+    console.log("creating menu");
 
+    let menu = new ClientMenu(
+      this.logger,
+      this.mainWindow,
+      dialog,
+      this.app,
+      this.system,
+      this.spawned_logs,
+      this.updater
+    );
+    menu.store = this.store;
+    let m = menu.makeMenu();
+  }
+  createUpdater() {
+    this.updater = new Updater(this.logger, this.mainWindow, dialog);
+    this.updater.defineUpdater();
+  }
+  createWindow() {
+    /**
+     * Initial window options
+     */
+    const $this = this;
 
-          const winURL = (process.env.NODE_ENV === 'development'
+    if (process.env.NODE_ENV !== "production") {
+      let icon = path.join(__dirname, "..", "static", "img");
+      if (process.env.platform_os == "linux") {
+        icon = path.join(icon, "/icon_1024x1024.png");
+      } else if (process.env.platform_os == "win") {
+        icon = path.join(icon, "/basestack.ico");
+      } else {
+        icon = path.join(icon, "/basestack.icns");
+      }
+      this.mainWindow = new BrowserWindow({
+        height: 1000,
+        useContentSize: true,
+        width: 1080,
+        title: "Basestack",
+        webPreferences: {
+          zoomFactor: 0.83,
+          webSecurity: true,
+          enableRemoteModule: true,
+          nodeIntegration: true,
+          worldSafeExecuteJavaScript: true,
+          contextIsolation: false,
+        },
+        icon: icon,
+      });
+      console.log(icon, __dirname);
+    } else {
+      let icon = path.join(__dirname, "icons");
+      if (process.env.platform_os == "linux") {
+        icon = path.join(icon, "/icon_1024x1024.png");
+      } else if (process.env.platform_os == "win") {
+        icon = path.join(icon, "/basestack.ico");
+      } else {
+        icon = path.join(icon, "/basestack.icns");
+      }
+      console.log(__dirname, "DIRNAME");
+      this.mainWindow = new BrowserWindow({
+        height: 1000,
+        useContentSize: true,
+        width: 1080,
+        title: "Basestack",
+        webPreferences: {
+          zoomFactor: 0.83,
+          webSecurity: true,
+          enableRemoteModule: true,
+          nodeIntegration: true,
+          worldSafeExecuteJavaScript: true,
+          contextIsolation: false,
+        },
+        icon: icon,
+      });
+    }
+
+    this.mainWindow.webContents.session.clearCache();
+
+    const winURL =
+      process.env.NODE_ENV === "development"
         ? `http://localhost:9080`
-        : `file://${__dirname}/index.html`);
-    
-        console.log("winurl defined")
-        this.mainWindow.webContents.session.clearCache(function(){
-        //some callback.
-        });
-        try{
-          this.mainWindow.loadURL(winURL)
-          
-        } catch(err){
-          console.log(err)
-        }
-        ipcMain.on("changePort", (event, arg) => {
-          process.env.PORT_SERVER = arg
-          event.reply('changePort', process.env.PORT_SERVER)
-        })
-        ipcMain.on("queryRelease", (event, arg) => {
-          event.reply('releaseNotes', $this.updater.releaseNotes)
-        })
-        ipcMain.on("openLogs", (event, arg) => {
-          shell.openPath($this.store.system.logPath ) 
-        })
-        
+        : `file://${__dirname}/index.html`;
 
+    console.log("winurl defined");
+    this.mainWindow.webContents.session.clearCache(function () {
+      //some callback.
+    });
+    try {
+      this.mainWindow.loadURL(winURL);
+    } catch (err) {
+      console.log(err);
+    }
+    ipcMain.on("changePort", (event, arg) => {
+      process.env.PORT_SERVER = arg;
+      event.reply("changePort", process.env.PORT_SERVER);
+    });
+    ipcMain.on("queryRelease", (event, arg) => {
+      event.reply("releaseNotes", $this.updater.releaseNotes);
+    });
+    ipcMain.on("openLogs", (event, arg) => {
+      shell.openPath($this.store.system.logPath);
+    });
 
-        ipcMain.on("checkUpdates", (event, arg) => {
-          console.log("Checking updates")
-          $this.updater.checkUpdates() 
+    ipcMain.on("checkUpdates", (event, arg) => {
+      console.log("Checking updates");
+      $this.updater.checkUpdates();
+    });
+    ipcMain.on("mainN", (event, arg) => {
+      // console.log(event)
+      console.log("event");
+    });
+    ipcMain.on("openDirSelect", (event, arg) => {
+      dialog
+        .showOpenDialog({
+          properties: ["openDirectory"],
         })
-        ipcMain.on("mainN", (event, arg) => {
-          // console.log(event)
-          console.log("event")
-        })
-        ipcMain.on("openDirSelect", (event, arg) => {
-          dialog.showOpenDialog({
-            properties: ['openDirectory']
-          }).then((val, err)=>{
-            if (err){
-              throw err
-            } else {
-              this.mainWindow.webContents.send("getValue", val.filePaths[0])
-            }
-            
-          }).catch((err)=>{
-            throw err
-          })
-          
-        })
-      
-      
-        this.mainWindow.webContents.on('did-finish-load', function () {
-          let quitUpdateInstall = false;
-          console.log("Basestack is finished loading")
-          function sendStatusToWindow(text) {
-            dialog.showMessageBox($this.mainWindow, {
-              type: 'info',
-              defaultId: 0,
-              buttons: ['Ok'],
-              message: 'Info',
-              detail: text
-            });
+        .then((val, err) => {
+          if (err) {
+            throw err;
+          } else {
+            this.mainWindow.webContents.send("getValue", val.filePaths[0]);
           }
         })
-        
-      
-        
-          
-        
-      
-      
-      
-        this.mainWindow.on("close", (e)=>{
-          // e.preventDefault();
-          
-          // try{
-          //   cancel_container({module: 'rampart', silent:true})
-          // } catch(err){
-          //   console.log(err)
-          // }
-          // try{
-          //     cancel_container({module: 'basestack_consensus', silent:true})
-          // } catch(err){
-          //   console.log(err)
-          // }
-          // try{
-          //     cancel_container({module: 'basestack_tutorial', silent: true})
-          // } catch(err){
-          //   console.log(err)
-          // }
-          // try{
-          //   cancel_container({module: 'basestack_mytax', silent:true})
-          // } catch(err){
-          //   console.log(err)
-          // }
-          //options object for dialog.showMessageBox(...)
-          let options = {}
-      
-          //Can be "none", "info", "error", "question" or "warning".
-          options.type = "question"
-      
-          //Array of texts for buttons.
-          options.buttons = ["Quit and Stop","Quit and Continue","Cancel"]
-      
-          //Index of the button in the buttons array which will be selected by default when the message box opens.
-          options.defaultId = 2
-      
-          //Title of the message box
-          options.title = "Multiple buttons on a message box"
-      
-          //Content of the message box
-          options.message = "Do you want to quit? "
-      
-          //More information of the message
-          options.detail = "Quit and keep your pipeline running for consensus generation, quit and stop them, or continue?"
-      
-      
-          //options.icon = "/path/image.png"
-      
-          //The index of the button to be used to cancel the dialog, via the Esc key
-          options.cancelId = 2
-      
-          //Prevent Electron on Windows to figure out which one of the buttons are common buttons (like "Cancel" or "Yes")
-          options.noLink = true
-      
-          //Normalize the keyboard access keys
-          // options.normalizeAccessKeys = true  
-          // dialog.showMessageBox(WIN, options, (response, checkboxChecked) => {
-          //   console.log(response)
-          //   console.log(checkboxChecked) //true or false
-          //  })
-      
-      
-      
-      
-        })
-      
-        this.mainWindow.on('closed', (e) => {
-          this.mainWindow= null
-          
-        })  
+        .catch((err) => {
+          throw err;
+        });
+    });
+
+    this.mainWindow.webContents.on("did-finish-load", function () {
+      let quitUpdateInstall = false;
+      console.log("Basestack is finished loading");
+      function sendStatusToWindow(text) {
+        dialog.showMessageBox($this.mainWindow, {
+          type: "info",
+          defaultId: 0,
+          buttons: ["Ok"],
+          message: "Info",
+          detail: text,
+        });
       }
-    checkUpdates(){
-        console.log("checking updates")
-        this.updater.checkUpdates()    
-    }
+    });
+
+    this.mainWindow.on("close", (e) => {
+      // e.preventDefault();
+
+      // try{
+      //   cancel_container({module: 'rampart', silent:true})
+      // } catch(err){
+      //   console.log(err)
+      // }
+      // try{
+      //     cancel_container({module: 'basestack_consensus', silent:true})
+      // } catch(err){
+      //   console.log(err)
+      // }
+      // try{
+      //     cancel_container({module: 'basestack_tutorial', silent: true})
+      // } catch(err){
+      //   console.log(err)
+      // }
+      // try{
+      //   cancel_container({module: 'basestack_mytax', silent:true})
+      // } catch(err){
+      //   console.log(err)
+      // }
+      //options object for dialog.showMessageBox(...)
+      let options = {};
+
+      //Can be "none", "info", "error", "question" or "warning".
+      options.type = "question";
+
+      //Array of texts for buttons.
+      options.buttons = ["Quit and Stop", "Quit and Continue", "Cancel"];
+
+      //Index of the button in the buttons array which will be selected by default when the message box opens.
+      options.defaultId = 2;
+
+      //Title of the message box
+      options.title = "Multiple buttons on a message box";
+
+      //Content of the message box
+      options.message = "Do you want to quit? ";
+
+      //More information of the message
+      options.detail =
+        "Quit and keep your pipeline running for consensus generation, quit and stop them, or continue?";
+
+      //options.icon = "/path/image.png"
+
+      //The index of the button to be used to cancel the dialog, via the Esc key
+      options.cancelId = 2;
+
+      //Prevent Electron on Windows to figure out which one of the buttons are common buttons (like "Cancel" or "Yes")
+      options.noLink = true;
+
+      //Normalize the keyboard access keys
+      // options.normalizeAccessKeys = true
+      // dialog.showMessageBox(WIN, options, (response, checkboxChecked) => {
+      //   console.log(response)
+      //   console.log(checkboxChecked) //true or false
+      //  })
+    });
+
+    this.mainWindow.on("closed", (e) => {
+      this.mainWindow = null;
+    });
+  }
+  checkUpdates() {
+    console.log("checking updates");
+    this.updater.checkUpdates();
+  }
 }
--- src/app/main/index.js
+++ src/app/main/index.js
@@ -1,39 +1,30 @@
-'use strict'
+"use strict";
 
-const { app, dialog } = require('electron')
+const { app, dialog } = require("electron");
 
+import promiseIpc from "electron-promise-ipc";
+const isMac = process.platform === "darwin";
+const isWin = process.platform === "win32";
 
-
-
-
-
-import promiseIpc from 'electron-promise-ipc';
-const isMac = process.platform === 'darwin'
-const isWin = process.platform === "win32"
-
-if (!process.env.APPDATA){ 
-  process.env.APPDATA = app.getPath('userData')
+if (!process.env.APPDATA) {
+  process.env.APPDATA = app.getPath("userData");
 }
-if (isMac){
-  process.env.platform_os = "mac"
-} else if(isWin){
-  process.env.platform_os = "win"
+if (isMac) {
+  process.env.platform_os = "mac";
+} else if (isWin) {
+  process.env.platform_os = "win";
 } else {
-  process.env.platform_os = "linux"
+  process.env.platform_os = "linux";
 }
 
 // process.env.NODE_ENV = 'production'
 
-const path = require("path")
+const path = require("path");
 // const {  show_MinKnow, show_sublime, show_aliview, show_spreaD3, show_BEAUTI, show_BEAST, show_figtree, show_tempest, show_tracer } = require('./menu.js')
 
-const {autoUpdater} = require("electron-updater");
+const { autoUpdater } = require("electron-updater");
 
-
-
-
-
-const {fs} = require("fs")
+const { fs } = require("fs");
 // autoUpdater.logger.transports.file.level = 'info';
 /**
  * Set `__static` path to static files in production
@@ -41,125 +32,127 @@ const {fs} = require("fs")
  */
 let releaseNotes;
 
-let os = require("os")
-if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
-  process.env.version_basestack = autoUpdater.currentVersion
+let os = require("os");
+if (process.env.NODE_ENV !== "development") {
+  global.__static = require("path")
+    .join(__dirname, "/static")
+    .replace(/\\/g, "\\\\");
+  process.env.version_basestack = autoUpdater.currentVersion;
   releaseNotes = {
     releaseNotes: "None Available",
     version: "Not Available",
-    releaseDate: "NA"
+    releaseDate: "NA",
   };
 } else {
-  process.env.version_basestack = "Development"
+  process.env.version_basestack = "Development";
   releaseNotes = {
     releaseNotes: "None Available",
     version: "Development",
-    releaseDate: "NA"
+    releaseDate: "NA",
   };
 }
-process.env.resourcesPath = process.resourcesPath
-  
-var {define_configuration } = require("../../shared/definitions.js")
-const { Client } = require("./client.js")
-const client = new Client(app)
- 
-let create_server; let close_server; let  cancel_container;
-if (process.env.NODE_ENV === 'production'){
-    create_server = require("../../server/index.server.js").create_server
-    // close_server = require("../../server/server.js").close_server
-    // const { 
-    //   cancel_container
-    //  } = require('../modules/server/api/controllers/index.js')
+process.env.resourcesPath = process.resourcesPath;
+
+var { define_configuration } = require("../../shared/definitions.js");
+const { Client } = require("./client.js");
+const client = new Client(app);
+
+let create_server;
+let close_server;
+let cancel_container;
+if (process.env.NODE_ENV === "production") {
+  create_server = require("../../server/index.server.js").create_server;
+  // close_server = require("../../server/server.js").close_server
+  // const {
+  //   cancel_container
+  //  } = require('../modules/server/api/controllers/index.js')
 }
-
-  
-
 
 let logger;
 
-
-
-client.app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+client.app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-}) 
+});
 
-client.app.on('activate', () => {
+client.app.on("activate", () => {
   if (client.mainWindow === null) {
-    client.createWindow()
-    client.createUpdater()
-    client.checkUpdates() 
-    client.createMenu()
+    client.createWindow();
+    client.createUpdater();
+    client.checkUpdates();
+    client.createMenu();
   }
-})
-client.app.on('ready', () => {
-    // if (process.env.NODE_ENV !== 'production') {
-    //   require('vue-devtools').install()
-    // }
-  
-  
+});
+client.app.on("ready", () => {
+  // if (process.env.NODE_ENV !== 'production') {
+  //   require('vue-devtools').install()
+  // }
+
   var { store } = require("../store/store.js");
   client.store = store;
-  define_configuration(store.system).then((config)=>{
-    try{
-      logger = require("./logger.js").logger(config.logs.error, config.logs.logfile)
-      process.env.logfile = config.logs.logfile
-      process.env.errorfile = config.logs.error
-      client.logger = logger;
-      if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
-      client.logger.info("Creating window")
-      client.createWindow()
-      client.logger.info("Creating updater")
-      client.createUpdater()
-      client.logger.info("Checking for updates...")
-      client.checkUpdates()
-      client.logger.info("Creating menu ")
-      client.createMenu(); 
-      try{
-        (async () => {
-          try{
-            client.logger.info("Checking node env for server")
-            if (process.env.NODE_ENV == 'production'){
-              client.logger.info("Production Mode detected, starting backend server...")
-              let port =  await create_server() 
-              if (!port ){
-                port = 5003
+  define_configuration(store.system)
+    .then((config) => {
+      try {
+        logger = require("./logger.js").logger(
+          config.logs.error,
+          config.logs.logfile
+        );
+        process.env.logfile = config.logs.logfile;
+        process.env.errorfile = config.logs.error;
+        client.logger = logger;
+        if (process.env.NODE_ENV === "production")
+          autoUpdater.checkForUpdates();
+        client.logger.info("Creating window");
+        client.createWindow();
+        client.logger.info("Creating updater");
+        client.createUpdater();
+        client.logger.info("Checking for updates...");
+        client.checkUpdates();
+        client.logger.info("Creating menu ");
+        client.createMenu();
+        try {
+          (async () => {
+            try {
+              client.logger.info("Checking node env for server");
+              if (process.env.NODE_ENV == "production") {
+                client.logger.info(
+                  "Production Mode detected, starting backend server..."
+                );
+                let port = await create_server();
+                if (!port) {
+                  port = 5003;
+                }
+                process.env.PORT_SERVER = port;
+                client.updatePort(port);
+                client.logger.info("Server started at port: %s", port);
               }
-              process.env.PORT_SERVER = port
-              client.updatePort(port)
-              client.logger.info("Server started at port: %s", port)
+            } catch (error) {
+              client.logger.info("%s error in readying the app", error);
+              client.logger.info(error);
+              // throw error
             }
-          } catch(error){
-            client.logger.info("%s error in readying the app", error)
-            client.logger.info(error)
-            // throw error
-          } 
-            
-          })().catch((err)=>{
-            client.logger.info("Error in ready app occurred somewhere, see above")
-            client.logger.info(err.message)
+          })().catch((err) => {
+            client.logger.info(
+              "Error in ready app occurred somewhere, see above"
+            );
+            client.logger.info(err.message);
             dialog.showMessageBox(client.mainWindow, {
-              type: 'error',
+              type: "error",
               defaultId: 0,
-              buttons: ['Ok'],
-              title: 'Error',
-              message: (err.message ? err.message : JSON.stringify(err, null, 4)),
+              buttons: ["Ok"],
+              title: "Error",
+              message: err.message ? err.message : JSON.stringify(err, null, 4),
+            });
           });
-        })
-      } catch(err){
-        console.error(err)
+        } catch (err) {
+          console.error(err);
+        }
+      } catch (err) {
+        console.error(err);
       }
-    } catch(err){
-      console.error(err)
-    }
-  }).catch((err)=>{
-    console.error(err)
-  })
-  
-})
-
-
-
-
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
